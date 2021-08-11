2148467c253c80efd7d62087d0200ee6223ef4b0
pabilokim <ej505904@gmail.com>
2021-08-12 02:44:51 +0900

    all

---
+++ insert.py
@@ -0,0 +1,23 @@
+import pymysql
+
+# 전역변수 선언부
+conn, cur = None, None
+data1, data2, data3, data4 = "", "", "", ""
+sql=""
+
+# 메인 코드
+conn = pymysql.connect( )
+cur = conn.cursor()
+
+while (True) :
+    data1 = input("사용자 ID ==> ")
+    if data1 == "" :
+        break;
+    data2 = input("사용자 이름 ==> ")
+    data3 = input("사용자 이메일 ==> ")
+    data4 = input("사용자 출생연도 ==> ")
+    sql = " "
+    cur.execute(sql)
+
+conn.commit()
+conn.close()
